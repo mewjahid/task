ec71f0fe531b1dfda617e2aa0353d6ba56b542bb
mewjahid <bloodxl91@gmail.com>
2019-06-26 00:39:13 +0300

    Удаление ненужной папки

--- config/db_params.php
+++ config/db_params.php
@@ -2,7 +2,7 @@
 
     return array(
         'host' => 'localhost',
-        'dbname' => 'm92835s1_app',
-        'user' => 'm92835s1_app',
-        'password' => 'cIum2kZI',
+        'dbname' => '',
+        'user' => '',
+        'password' => '',
     );
